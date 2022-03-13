2400d18b20e6c5fd528b6f2d1fee881a5d565029
KEENTO <handball0321@gmail.com>
2022-03-13 14:28:01 -0700

    Add route (home,redirect,not found)

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p>This is home page. This text is dummy.</p>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,22 @@
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
+import Home from "./Home";
+import NotFound from "./NotFound";
+
 const Main = () => {
   return (
-    <div>
-      <p>Dummy text</p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 

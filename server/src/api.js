b7d2c94576f182334982e5a1e6843d52debc3186
KEENTO <handball0321@gmail.com>
2022-04-21 20:38:33 -0700

    Cors opt

--- server/src/api.js
+++ server/src/api.js
@@ -9,7 +9,11 @@ const stripe = require("stripe")("sk_test_Hrs6SAopgFPF0bZXSN3f6ELN");
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://freshmarche-dwi3a9d45-keento0809.vercel.app/",
+};
+app.use(cors(corsOptions));
 
 const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
--- src/stripe/Stripe.jsx
+++ src/stripe/Stripe.jsx
@@ -20,14 +20,11 @@ export default function Stripe() {
   useEffect(() => {
     setIsLoading(true);
     // Create PaymentIntent as soon as the page loads
-    fetch(
-      "https://626219fa9e21ec15c2306066--coruscating-parfait-87eda0.netlify.app/.netlify/functions/api/create-payment-intent",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-      }
-    )
+    fetch("/.netlify/functions/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
       .then((res) => res.json())
       .then((data) => {
         setClientSecret(data.clientSecret);

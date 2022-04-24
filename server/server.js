3ad1e5fd2c0369d38c103f83a12f36e082b7f175
KEENTO <handball0321@gmail.com>
2022-04-24 16:10:23 -0700

    Final fixment

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
 const app = express();
 
-const corsOptions = {
-  // original code
-  // origin: "https://server-retry.herokuapp.com",
-  origin: "https://freshmarche-server.herokuapp.com",
-};
+// const corsOptions = {
+//   // original code
+//   // origin: "https://server-retry.herokuapp.com",
+//   origin: "https://freshmarche-server.herokuapp.com",
+// };
 
-app.use(cors(corsOptions));
-require("dotenv").config();
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
 const stripe = require("stripe")("sk_test_Hrs6SAopgFPF0bZXSN3f6ELN");
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
--- src/stripe/Stripe.jsx
+++ src/stripe/Stripe.jsx
@@ -22,18 +22,15 @@ export default function Stripe() {
     // Create PaymentIntent as soon as the page loads
     // original code
     // fetch("https://server-retry.herokuapp.com/create-payment-intent", {
-    fetch("https://freshmarche-server.herokuapp.com/create-payment-intent", {
+    fetch("https://server-retry.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
     })
       // .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
-        setClientSecret(data.clientSecret);
+        setClientSecret(data?.clientSecret);
         setIsLoading(false);
       });
   }, []);
